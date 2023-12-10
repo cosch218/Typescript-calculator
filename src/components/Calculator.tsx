3e25716ce4c31d8bee38b7ef325ced5e076ad75a
cosch218 <cosch218@gmail.com>
2023-12-10 23:53:57 +0900

    1210 계산기 기능 작성

--- src/components/Calculator.tsx
+++ src/components/Calculator.tsx
@@ -1,63 +1,121 @@
 import React, { useState } from 'react'
 
 export default function Calculator() {
-    const [number, setNumber] = useState<number | null>(999999999);
 
-    const handleButtonNumber = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        const buttonValue = e.currentTarget.value;
-        if (buttonValue === "null") {
-            setNumber(null)
+    // 계산식 상태관리
+    const [calc, setCalc] = useState<string>("");
+
+    // 입력한 버튼값 상태관리
+    const [btnNum, setBtnNum] = useState<string>("");
+    
+    const [plusMinus, setPlusMinus] = useState<boolean>(true);
+
+    const handleReset = () => {
+        setCalc("");
+    }
+
+    const handlePlusMinus = () => {
+        setPlusMinus(!plusMinus);
+        
+        if (plusMinus) {
+            // 양수로 전환
+            const calcToNum = parseFloat(calc);
+            const toPositiveNum = Math.abs(calcToNum);
+            setCalc(toPositiveNum.toString());
         } else {
-            setNumber(parseFloat(buttonValue));
+            // 음수로 전환
+            setCalc((prev) => "-" + prev)
+        }
+    }
+
+    const handlePercent = () => {
+        const calcToNum = parseFloat(calc);
+        const toPercent = calcToNum/100;
+        setCalc(toPercent.toString());
+    }
+
+    const getButtonValue = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const clickedBtnValue = e.currentTarget.value;
+        
+        // 만약 소수점을 한 번 클릭했다면 추가로 더 입력할 수 없음
+        if ( btnNum.includes(".") && clickedBtnValue === ".") {
+            alert(".을 하나의 숫자에 두 번 이상 입력할 수 없습니다!");
+        } else {
+            setCalc((prev) => prev + clickedBtnValue);
+            setBtnNum((prev) => prev + clickedBtnValue);
         }
     };
 
-    const formattedNumberWithCommas = (number: number) => {
-        return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    const getButtonOper = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        const clickedBtnOper = e.currentTarget.value;
+        setCalc((prev) => prev + clickedBtnOper);
+        setBtnNum("");
+    };
+
+    const getResult = () => {
+        const replaceOper = calc.replace(/÷/gi, "/").replace(/×/gi, "*");
+
+        const calcResult = eval(replaceOper)
+
+        // 만약 calcResult가 소수점 두 자리 이상이라면
+        if (/^\d+\.\d{2,}$/.test(calcResult.toString())) {
+            const calcResultToNum = parseFloat(calcResult).toFixed(2)
+            setCalc(calcResultToNum.toString())
+        } else {
+            setCalc(calcResult)
+        }
+        
+        return calc
     }
+
+    const formattedNumberWithCommas = (result: string) => {
+        return result.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    };
+
 
     return (
         <div>
             <p className='result'>
                 {
-                    number !== null && (number.toString().length < 10)
-                    ? formattedNumberWithCommas(number) : 0
+                    formattedNumberWithCommas(calc).length < 12
+                    ? formattedNumberWithCommas(calc)
+                    : "--"
                 }
             </p>
 
             <div className='calculator'>
                 <div>
-                    <button className='lightgray' value={"null"} onClick={handleButtonNumber}>AC</button>
-                    <button className='lightgray'>±</button>
-                    <button className='lightgray'>%</button>
-                    <button className='orange'>÷</button>
+                    <button className='lightgray' onClick={handleReset}>AC</button>
+                    <button className='lightgray' onClick={handlePlusMinus}>±</button>
+                    <button className='lightgray' onClick={handlePercent}>%</button>
+                    <button className='orange' value="÷" onClick={getButtonOper}>÷</button>
                 </div>
 
                 <div>
-                    <button className='gray' value={7} onClick={handleButtonNumber}>7</button>
-                    <button className='gray' value={8} onClick={handleButtonNumber}>8</button>
-                    <button className='gray' value={9} onClick={handleButtonNumber}>9</button>
-                    <button className='orange'>×</button>
+                    <button className='gray' value={7} onClick={getButtonValue}>7</button>
+                    <button className='gray' value={8} onClick={getButtonValue}>8</button>
+                    <button className='gray' value={9} onClick={getButtonValue}>9</button>
+                    <button className='orange' value="×" onClick={getButtonOper}>×</button>
                 </div>
 
                 <div>
-                    <button className='gray' value={4} onClick={handleButtonNumber}>4</button>
-                    <button className='gray' value={5} onClick={handleButtonNumber}>5</button>
-                    <button className='gray' value={6} onClick={handleButtonNumber}>6</button>
-                    <button className='orange'>－</button>
+                    <button className='gray' value={4} onClick={getButtonValue}>4</button>
+                    <button className='gray' value={5} onClick={getButtonValue}>5</button>
+                    <button className='gray' value={6} onClick={getButtonValue}>6</button>
+                    <button className='orange' value="-" onClick={getButtonOper}>-</button>
                 </div>
 
                 <div>
-                    <button className='gray' value={1} onClick={handleButtonNumber}>1</button>
-                    <button className='gray' value={2} onClick={handleButtonNumber}>2</button>
-                    <button className='gray' value={3} onClick={handleButtonNumber}>3</button>
-                    <button className='orange'>＋</button>
+                    <button className='gray' value={1} onClick={getButtonValue}>1</button>
+                    <button className='gray' value={2} onClick={getButtonValue}>2</button>
+                    <button className='gray' value={3} onClick={getButtonValue}>3</button>
+                    <button className='orange' value="+" onClick={getButtonOper}>+</button>
                 </div>
 
                 <div>
-                    <button className='gray' value={0} onClick={handleButtonNumber}>0</button>
-                    <button className='gray'>.</button>
-                    <button className='orange'>＝</button>
+                    <button className='gray' value={0} onClick={getButtonValue}>0</button>
+                    <button className='gray' value="." onClick={getButtonValue}>.</button>
+                    <button className='orange' value="＝" onClick={getResult}>＝</button>
                 </div>
             </div>
         </div>
